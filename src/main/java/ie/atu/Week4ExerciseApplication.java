a6055e0a42c3f0574e9532182ad9da32ee7df5b7
denis <g00403126@atu.ie>
2023-10-22 23:35:17 +0100

    ...

---
+++ src/main/java/ie/atu/Week4ExerciseApplication.java
@@ -0,0 +1,13 @@
+package ie.atu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week4ExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week4ExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/atu/Week4ExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week4ExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
